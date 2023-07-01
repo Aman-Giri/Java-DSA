ae652c15401994578b287f0498537646677c8b67
Aman Giri <amangirikg@gmail.com>
2023-07-01 23:21:05 +0530

    added the code

---
+++ Arrays/ArraysExample.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class ArraysExample {
+    public static void main(String[] args) {
+        String name = "Aman Giri";
+        String name1 = "sunny";
+
+        int roll1 = 23;
+        int roll2 = 33;
+        int roll3 = 55;
+
+         
+        int[] rollnumbers; // rollnumbers is the ref variable which is storing into the stack memory
+        rollnumbers = new int[4]; //object is created into the heap memory 
+
+        // int[] arr = new int[4]; // Syntax for defining the array in java
+
+        System.out.println("Integer Array: " + rollnumbers[0] );
+
+        //String Array: 
+        String[] names = new String[3];
+        System.out.println("String Array: " + names[1]);
+        
+    }
+}
---
+++ Arrays/ArraysInput.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraysInput {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] arr = new int[4];
+
+        System.out.println("Enter the element of the array: ");
+
+        //for loop for taking the array input from the user
+        for(int i=0;i<arr.length;i++){
+            arr[i] = input.nextInt();
+        }
+
+        //For loop for iterating the element of the array
+        for(int i=0;i<arr.length;i++){
+            System.out.println("The element of the array you entered are: "+ arr[i]);
+        }
+
+        //To print the the array element into the String format
+        System.out.println("The Arrays are: " + Arrays.toString(arr));
+    }
+}
