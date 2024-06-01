d111d23c86104e0d5881e99d9f7d3b8a42f7315e
“Aman-Giri” <Amangirikg@gmail.com>
2024-06-02 00:57:13 +0530

    LinearSearch Questions commit

---
+++ LinearSearch/FindNumberswithEvenNumberofDigits.java
@@ -0,0 +1,9 @@
+package LinearSearch;
+
+public class FindNumberswithEvenNumberofDigits {
+    public static void main(String[] args) {
+        
+    }
+
+    //function to find the number with even numberr digit
+}
